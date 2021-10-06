eaa8919318f50e5aef97c39abfdce98d0f2469f6
PietrangeloManco <p.manco@studenti.unipi.it>
2021-10-06 13:30:28 +0200

    Added the basic stats.

--- Primo/primo.py
+++ Primo/primo.py
@@ -26,17 +26,20 @@ def check(path):
 
 def characters_counter(bookpath):
     """Actual counter of occurrences."""
-    letters_list = book_creation(bookpath)
-    counter = Counter(letters_list)
-    occurrences_sum = sum(counter.values())
-    occurrences_dict = sorted(counter.items())
+    letters_list, lines_list, words_number = book_creation(bookpath)
+    letters_counter = Counter(letters_list)
+    occurrences_sum = sum(letters_counter.values())
+    occurrences_dict = sorted(letters_counter.items())
     for key, value in occurrences_dict:
         print(f'{key} : {value*100/occurrences_sum:.4f} % ({value} occurrences)')
+    print('\nDo you want to see the given book stats? (y for yes, anything else for no).\n')
+    if input() == 'y':
+        basic_stats(occurrences_sum, lines_list, words_number)
     return occurrences_dict
 
 def histogram(occ):
     """Making an histogram of the occurrences."""
-    print('\nDo you want to see the occurrences histogram? (y for yes, anything else for no.)\n')
+    print('\nDo you want to see the occurrences histogram? (y for yes, anything else for no).\n')
     if input() == 'y':
         hist = pandas.DataFrame(occ, columns = ['letter', 'frequency'])
         hist.plot(kind = 'bar', x = 'letter')
@@ -46,12 +49,12 @@ def histogram(occ):
 
 def book_lenght(text):
     """Asking the user if he wants to skip some pages."""
-    print('\nDo you want to skip some lines? Type "y" for yes, anything else for no.\n')
+    print('\nDo you want to skip some lines? (y for yes, anything else for no).\n')
     if input() == 'y':
         print('\nWhich line should I start with?\n')
         while True:
             try:
-                init = int(input())
+                init = int(input()) - 1
                 break
             except ValueError:
                 print('\nInvalid choice. Try again.\n')
@@ -59,7 +62,7 @@ def book_lenght(text):
             print('\nWhich line shoud I stop with?\n')
             while True:
                 try:
-                    last = int(input())
+                    last = int(input()) - 1
                     break
                 except ValueError:
                     print('\nInvalid choice. Try again.\n')
@@ -73,12 +76,25 @@ def book_creation(bpath):
     with open(bpath) as book:
         book_lines = book.readlines()
         effective_book_lines = book_lenght(book_lines)
+    wordcount = 0
+    for line in effective_book_lines:
+        wordcount += len(line.split())
     with open('New_book.txt', 'w') as effective_book_w:
         for item in effective_book_lines:
             effective_book_w.write('%s\n' % item)
     with open('New_book.txt', 'r') as  effective_book_r:
         ext_letters_list = [ch.lower() for ch in effective_book_r.read() if ch.isalpha()]
-    return ext_letters_list
+    return [ext_letters_list, effective_book_lines, wordcount]
+
+def basic_stats(book_characters, book_lines, book_words):
+    """Some prints of book stats."""
+    num_of_lines = 0
+    for item in book_lines:
+        if item != '\n':
+            num_of_lines += 1
+    print(f'\nThe number of characters is {book_characters}.\n')
+    print(f'\nThe number of non blank lines is {num_of_lines}.\n')
+    print(f'\nThe number of words is {book_words}.\n')
 
 time_0 = timer()
 while True:
