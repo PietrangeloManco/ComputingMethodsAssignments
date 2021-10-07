7498c262f9199f310f0e79ab08bef60ce306f675
PietrangeloManco <p.manco@studenti.unipi.it>
2021-10-07 13:31:46 +0200

    argparse implemented

--- Primo/primo.py
+++ Primo/primo.py
@@ -1,81 +1,48 @@
 """Counts the occurrences of each alphabetical character in a given book."""
 from collections import Counter
 from timeit import default_timer as timer
-import os
-from matplotlib import pyplot as plt
+import argparse
+import sys
 import pandas
-
-def start():
-    """--help option function."""
-    print('\nType "--help" for instructions, "run" to execute the script.\n')
-    choice = input()
-    if choice == '--help':
-        print('\nInsert a txt file path to get the letters occurences.\n')
-        return 'ok'
-    if choice == 'run':
-        return 'ok'
-    print('\nInvalid choice. Try again.\n')
-    return 'ohno'
+from matplotlib import pyplot as plt
 
 def check(path):
     """Checking the input file's validity."""
-    if os.path.exists(path) and path.endswith('.txt'):
+    if path.endswith('.txt'):
         return 'nice'
     print('\nBad or nonexisting file. Try again.\n')
     return 'abort'
 
-def characters_counter(bookpath):
+def characters_counter(letters_list):
     """Actual counter of occurrences."""
-    letters_list, lines_list, words_number = book_creation(bookpath)
     letters_counter = Counter(letters_list)
     occurrences_sum = sum(letters_counter.values())
     occurrences_dict = sorted(letters_counter.items())
     for key, value in occurrences_dict:
         print(f'{key} : {value*100/occurrences_sum:.4f} % ({value} occurrences)')
-    print('\nDo you want to see the given book stats? (y for yes, anything else for no).\n')
-    if input() == 'y':
-        basic_stats(occurrences_sum, lines_list, words_number)
-    return occurrences_dict
+    return occurrences_dict, occurrences_sum
 
 def histogram(occ):
     """Making an histogram of the occurrences."""
-    print('\nDo you want to see the occurrences histogram? (y for yes, anything else for no).\n')
-    if input() == 'y':
-        hist = pandas.DataFrame(occ, columns = ['letter', 'frequency'])
-        hist.plot(kind = 'bar', x = 'letter')
-        plt.show()
-        return
-    return
+    hist = pandas.DataFrame(occ, columns = ['letter', 'frequency'])
+    hist.plot(kind = 'bar', x = 'letter')
+    plt.show()
 
-def book_lenght(text):
+def book_lenght(text, fline, lline):
     """Asking the user if he wants to skip some pages."""
-    print('\nDo you want to skip some lines? (y for yes, anything else for no).\n')
-    if input() == 'y':
-        print('\nWhich line should I start with?\n')
-        while True:
-            try:
-                init = int(input()) - 1
-                break
-            except ValueError:
-                print('\nInvalid choice. Try again.\n')
-        if 0 < init < len(text):
-            print('\nWhich line shoud I stop with?\n')
-            while True:
-                try:
-                    last = int(input()) - 1
-                    break
-                except ValueError:
-                    print('\nInvalid choice. Try again.\n')
-            if last > 0 and init < last < len(text):
-                effective_text = text[init:last]
-                return effective_text
-    return text
+    if lline == -1:
+        lline = len(text)
+    if 0 <= fline <= lline <= len(text):
+        effective_text = text[fline:lline]
+        return effective_text
+    print('\nInvalide line choices.\n')
+    sys.exit()
 
-def book_creation(bpath):
+def book_creation(bpath, firstline, lastline):
     """Takes a book and does stuff with it."""
     with open(bpath) as book:
         book_lines = book.readlines()
-        effective_book_lines = book_lenght(book_lines)
+        effective_book_lines = book_lenght(book_lines, firstline, lastline)
     wordcount = 0
     for line in effective_book_lines:
         wordcount += len(line.split())
@@ -96,16 +63,21 @@ def basic_stats(book_characters, book_lines, book_words):
     print(f'\nThe number of non blank lines is {num_of_lines}.\n')
     print(f'\nThe number of words is {book_words}.\n')
 
+parser = argparse.ArgumentParser()
+parser.add_argument("book", type = str, help = "Book file to work on.")
+parser.add_argument("-histogram", "-hi", help = "Shows the histogram plot.", action = "store_true")
+parser.add_argument("-stats", "-s", help = "Shows the books stats.", action = "store_true")
+parser.add_argument("-startb", "-st", type = int, default = 0, help = "Read the book from here.")
+parser.add_argument("-endb", "-e", type = int, default = -1, help = "Read the book ending here.")
+args = parser.parse_args()
 time_0 = timer()
-while True:
-    if start() == 'ok':
-        break
-print('\nInsert your files path:\n')
-while True:
-    book_path = (input())
-    if check(book_path) == 'nice':
-        break
-oc_dict = characters_counter(book_path)
-histogram(oc_dict)
+if check(args.book) == 'abort':
+    sys.exit()
+lett_list, l_list, w_number = book_creation(args.book, args.startb, args.endb)
+oc_dict, oc_sum = characters_counter(lett_list)
+if args.histogram:
+    histogram(oc_dict)
+if args.stats:
+    basic_stats(oc_sum, l_list, w_number)
 time_1 = timer()
 print(f'\nTotal elapsed time: {(time_1-time_0): .4f} s\n')
